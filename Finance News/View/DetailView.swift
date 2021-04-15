1dd61693290d044a41c9ad7f7ae62556ecf75719
youngjun kim <youngjun.kim14@gmail.com>
2021-04-15 23:18:20 +0900

    Refactoring

--- Finance News/View/DetailView.swift
+++ Finance News/View/DetailView.swift
@@ -23,21 +23,4 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeUrl = urlString {
-            if let url = URL(string: safeUrl) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-            
-        }
-    }
-}
+
---
+++ Finance News/View/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  Finance News
+//
+//  Created by youngjun kim on 2021/04/15.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrl = urlString {
+            if let url = URL(string: safeUrl) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+            
+        }
+    }
+}
