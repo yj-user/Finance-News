2e5bd25523ce1bd16996aa4981859e78342f2dca
youngjun kim <youngjun.kim14@gmail.com>
2021-04-15 14:35:04 +0900

    Initial Commit

---
+++ Finance News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance News
+//
+//  Created by youngjun kim on 2021/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
